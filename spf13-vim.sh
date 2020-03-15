a06e95c0b527744efff9b20ea279b603e81ac7d1
grepsun <405451312@qq.com>
2020-03-15 11:32:44 +0800

    安装脚本支持fork配置

--- spf13-vim.sh
+++ spf13-vim.sh
@@ -19,8 +19,8 @@ app_name='spf13-vim'
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/.spf13-vim-3"
 [ -z "$REPO_URI" ] && REPO_URI='https://github.com/szxdlut/spf13-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
-debug_mode='0'
-fork_maintainer='0'
+debug_mode='1'
+fork_maintainer='1'
 [ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
 
 ############################  BASIC SETUP TOOLS
